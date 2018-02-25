7c16d4e5214ade715175955866929c91c447c878
lidor <648799160@qq.com>
2018-02-25 14:48:51 +0800

    后台管理系统添加内容管理的redis同步缓存（仅实现新增操作的缓存）

--- youtu-manager-service/src/main/java/com/youtu/service/impl/ContentServiceImpl.java
+++ youtu-manager-service/src/main/java/com/youtu/service/impl/ContentServiceImpl.java
@@ -3,29 +3,45 @@ package com.youtu.service.impl;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.youtu.common.pojo.YouTuResult;
+import com.youtu.common.utils.HttpClientUtil;
 import com.youtu.mapper.TbContentMapper;
 import com.youtu.pojo.TbContent;
 import com.youtu.service.ContentService;
 
-/**内容管理
- *@author:张晓芬
- *@date:2018年2月22日  下午10:33:18
-**/
+/**
+ * 内容管理
+ * 
+ * @author:张晓芬
+ * @date:2018年2月22日 下午10:33:18
+ **/
 @Service
 public class ContentServiceImpl implements ContentService {
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
+
 	@Override
 	public YouTuResult insertContent(TbContent content) {
-		//补全pojo内容
-				content.setCreated(new Date());
-				content.setUpdated(new Date());
-				contentMapper.insert(content);
-				
-				return YouTuResult.ok();
+		// 补全pojo内容
+		content.setCreated(new Date());
+		content.setUpdated(new Date());
+		contentMapper.insert(content);
+		
+		// redis同步缓存
+		try {
+			HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return YouTuResult.ok();
 	}
 
 }
