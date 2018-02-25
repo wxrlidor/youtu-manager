da379bb82d2eac0ead82230f066c5d2c63c84dd0
lidor <648799160@qq.com>
2018-02-25 21:08:04 +0800

    修改tbItem.java，使得可以获取多张图片images，适应门户的商品详情展示功能

--- youtu-manager-pojo/src/main/java/com/youtu/pojo/TbItem.java
+++ youtu-manager-pojo/src/main/java/com/youtu/pojo/TbItem.java
@@ -3,119 +3,136 @@ package com.youtu.pojo;
 import java.util.Date;
 
 public class TbItem {
-    private Long id;
+	private Long id;
 
-    private String title;
+	private String title;
 
-    private String sellPoint;
+	private String sellPoint;
 
-    private Long price;
+	private Long price;
 
-    private Integer num;
+	private Integer num;
 
-    private String barcode;
+	private String barcode;
 
-    private String image;
+	private String image;
 
-    private Long cid;
+	private Long cid;
 
-    private Byte status;
+	private Byte status;
 
-    private Date created;
+	private Date created;
 
-    private Date updated;
-    
-    private String catName;
-    
-    public Long getId() {
-        return id;
-    }
+	private Date updated;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	private String catName;
 
-    public String getTitle() {
-        return title;
-    }
+	private String[] images;
 
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
+	public String[] getImages() {
+		if (image != null) {
+			// 根据逗号分隔多个图片路径，存储在字符串数组中
+			String[] images = image.split(",");
+			return images;
+		}
+		return null;
+	}
 
-    public String getSellPoint() {
-        return sellPoint;
-    }
-    public void setCatName(String catName) {
+	public void setImages(String[] images) {
+		this.images = images;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title == null ? null : title.trim();
+	}
+
+	public String getSellPoint() {
+		return sellPoint;
+	}
+
+	public void setCatName(String catName) {
 		this.catName = catName;
 	}
-    public void setSellPoint(String sellPoint) {
-        this.sellPoint = sellPoint == null ? null : sellPoint.trim();
-    }
 
-    public Long getPrice() {
-        return price;
-    }
+	public void setSellPoint(String sellPoint) {
+		this.sellPoint = sellPoint == null ? null : sellPoint.trim();
+	}
 
-    public void setPrice(Long price) {
-        this.price = price;
-    }
+	public Long getPrice() {
+		return price;
+	}
 
-    public Integer getNum() {
-        return num;
-    }
+	public void setPrice(Long price) {
+		this.price = price;
+	}
 
-    public void setNum(Integer num) {
-        this.num = num;
-    }
+	public Integer getNum() {
+		return num;
+	}
 
-    public String getBarcode() {
-        return barcode;
-    }
+	public void setNum(Integer num) {
+		this.num = num;
+	}
 
-    public void setBarcode(String barcode) {
-        this.barcode = barcode == null ? null : barcode.trim();
-    }
+	public String getBarcode() {
+		return barcode;
+	}
 
-    public String getImage() {
-        return image;
-    }
+	public void setBarcode(String barcode) {
+		this.barcode = barcode == null ? null : barcode.trim();
+	}
 
-    public void setImage(String image) {
-        this.image = image == null ? null : image.trim();
-    }
+	public String getImage() {
+		return image;
+	}
 
-    public Long getCid() {
-        return cid;
-    }
+	public void setImage(String image) {
+		this.image = image == null ? null : image.trim();
+	}
 
-    public void setCid(Long cid) {
-        this.cid = cid;
-    }
+	public Long getCid() {
+		return cid;
+	}
 
-    public Byte getStatus() {
-        return status;
-    }
+	public void setCid(Long cid) {
+		this.cid = cid;
+	}
 
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
+	public Byte getStatus() {
+		return status;
+	}
 
-    public Date getCreated() {
-        return created;
-    }
+	public void setStatus(Byte status) {
+		this.status = status;
+	}
 
-    public void setCreated(Date created) {
-        this.created = created;
-    }
+	public Date getCreated() {
+		return created;
+	}
 
-    public Date getUpdated() {
-        return updated;
-    }
+	public void setCreated(Date created) {
+		this.created = created;
+	}
 
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
 
 	public String getCatName() {
 		return catName;
